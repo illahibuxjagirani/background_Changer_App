333abb4d1ccb5305e32f0f90346c3316325aaa0e
illahibuxjagirani <illahibuxjagirani@gmail.com>
2024-04-25 11:37:40 +0500

    back to home button

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,15 @@ let [bg, setBg] = useState("")
 let [color, setColor] = useState("text-white");
 let [text, setText] = useState('')
 let white = "text-white";
+let [welcome, setWelcome] = useState("")
   return (
    <>
-   <div className={`${bg} h-screen w-full flex flex-col justify-between items-center py-10 transition-bg duration-700`}>
-    <h1 className={`${color} text-center font-bold text-3xl`}>Welcome to IB Coding School
+   <div className={`${bg} h-screen w-full flex flex-col justify-between items-center py-28 transition-bg duration-700`}>
+    <h1 className={`${color} text-center font-bold text-3xl`}>{welcome}
     <br /> <br /> <br /> {text} 
     </h1>
 
-    <div className='bg-slate-300 w-[60%] p-4 rounded-xl gap-2 flex justify-between flex-wrap lg:flex-nowrap sm:text-sm sm:gap-1'>
+    <div className='bg-slate-300 w-[90%] p-4 rounded-xl gap-2 flex justify-between flex-wrap lg:flex-nowrap sm:text-sm sm:gap-1'>
 
       <button 
 
@@ -23,6 +24,7 @@ let white = "text-white";
           setBg("bg-red-700");
           setColor(white);
           setText("Welcome to Red Color Background")
+          setWelcome("IB Coding School Projects")
         }}
          className='bg-red-700 text-white px-4 py-1 rounded-xl font-bold hover:bg-red-800'>Red</button>
 
@@ -32,6 +34,7 @@ let white = "text-white";
           setBg("bg-green-700");
           setColor(white);
           setText("Welcome to GREEN Color Background") 
+          setWelcome("IB Coding School Projects")
         }}
          className='bg-green-700 text-white px-4 py-1 rounded-xl font-bold hover:bg-green-800'>Green</button>
 
@@ -41,6 +44,7 @@ let white = "text-white";
           setBg("bg-blue-700");
           setColor(white);
           setText("Welcome to BLUE Color Background") 
+          setWelcome("IB Coding School Projects")
         }}
          className='bg-blue-700 text-white px-4 py-1 rounded-xl font-bold hover:bg-blue-800'>Blue</button>
 
@@ -50,6 +54,7 @@ let white = "text-white";
           setBg("bg-orange-700");
           setColor(white);
           setText("Welcome to ORANGE Color Background") 
+          setWelcome("IB Coding School Projects")
         }}
          className='bg-orange-700 text-white px-4 py-1 rounded-xl font-bold hover:bg-orange-800'>Orange</button>
 
@@ -59,6 +64,7 @@ let white = "text-white";
           setBg("bg-gray-700");
           setColor(white);
           setText("Welcome to GRAY Color Background") 
+          setWelcome("IB Coding School Projects")
         }}
          className='bg-gray-700 text-white px-4 py-1 rounded-xl font-bold hover:bg-gray-800'>Gray</button>
 
@@ -68,6 +74,7 @@ let white = "text-white";
           setBg("bg-yellow-700");
           setColor(white);
           setText("Welcome to YELLOW Color Background") 
+          setWelcome("IB Coding School Projects")
         }}
           className='bg-yellow-700 text-white px-4 py-1 rounded-xl font-bold hover:bg-yellow-800'>Yellow</button>
 
@@ -77,6 +84,7 @@ let white = "text-white";
           setBg("bg-pink-700");
           setColor(white);
           setText("Welcome to PINK Color Background") 
+          setWelcome("IB Coding School Projects")
         }}
           className='bg-pink-700 text-white px-4 py-1 rounded-xl font-bold hover:bg-pink-800'>Pink</button>
 
@@ -86,6 +94,7 @@ let white = "text-white";
           setBg("bg-purple-700");
           setColor(white);
           setText("Welcome to PURPLE Color Background") 
+          setWelcome("IB Coding School Projects")
         }}
           className='bg-purple-700 text-white px-4 py-1 rounded-xl font-bold hover:bg-purple-800'>Purple</button>
 
@@ -95,6 +104,7 @@ let white = "text-white";
           setBg("bg-teal-700");
           setColor(white);
           setText("Welcome to TEAL Color Background")
+          setWelcome("IB Coding School Projects")
         }}
           className='bg-teal-700 text-white px-4 py-1 rounded-xl font-bold hover:bg-teal-800'>Teal</button>
 
@@ -104,6 +114,7 @@ let white = "text-white";
           setBg("bg-white");
           setColor(!color);
           setText("Welcome to WHITE Color Background") 
+          setWelcome("IB Coding School Projects")
         }} 
         className='bg-white text-black px-4 py-1 rounded-xl font-bold hover:bg-slate-200'>White</button>
 
@@ -113,10 +124,17 @@ let white = "text-white";
           setBg("bg-black");
           setColor(white);
           setText("Welcome to BLACK Color Background") 
+          setWelcome("IB Coding School Projects")
         }}
          className='bg-black text-white px-4 py-1 rounded-xl font-bold hover:bg-slate-800'>Black</button>
+        <br />
+      
     </div>
-
+    <button  
+      onClick={
+        () => { window.location.reload()
+        }}
+         className='bg-black text-white px-4 py-1 rounded-xl font-bold hover:bg-slate-800'>Back to Home</button>
    </div>
    </>
   )
